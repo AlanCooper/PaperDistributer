7acca49d2f3bbc2aa28f0fd7816801a322436c96
AlanCooper <liujuncheng1022@gmail.com>
2010-11-13 16:21:31 +0800

    nothing

--- PaperDistributer/main.cpp
+++ PaperDistributer/main.cpp
@@ -1,7 +1,10 @@
 #include <QApplication>
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    MainWindow mainWindows(0);
+    mainWindows.show();
     return app.exec();
 }
